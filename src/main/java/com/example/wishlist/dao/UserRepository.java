66c01cfae9b9c729250606ce66f1cf84d1f96fec
mads94i5 <mads94i5@stud.kea.dk>
2022-10-24 14:38:21 +0200

    Connector & User + UserRepo

--- src/main/java/com/example/wishlist/dao/UserRepository.java
+++ src/main/java/com/example/wishlist/dao/UserRepository.java
@@ -1,8 +1,10 @@
 package com.example.wishlist.dao;
 
 import com.example.wishlist.ents.User;
+import com.example.wishlist.ents.Wishlist;
 import org.springframework.stereotype.Repository;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,7 +12,59 @@ import java.util.List;
 public class UserRepository {
     public List<User> getAll() {
         List<User> users = new ArrayList<>();
+        try {
+            Connection conn = new Connector().getConnection();
 
+            PreparedStatement psts = conn.prepareStatement("SELECT * FROM users");
+            ResultSet resultSet = psts.executeQuery();
+
+            while (resultSet.next()) {
+                Long id = resultSet.getLong(1);
+                String userName = resultSet.getString(2);
+                String password = resultSet.getString(3);
+                int wishlistId = resultSet.getInt(4);
+                users.add(new User(id, userName, password, wishlistId));
+            }
+        } catch (SQLException e) {
+            System.out.println("Cannot connect to database.");
+            e.printStackTrace();
+        }
         return users;
+    }
+    public void create(User newUser) {
+        try {
+            Connection conn = new Connector().getConnection();
+
+            String query = "INSERT INTO users (user_name, password, wishlist_id) VALUES (?, ?, ?)";
+            PreparedStatement psts = conn.prepareStatement(query);
+
+            psts.setString(1, newUser.getUserName());
+            psts.setString(2, newUser.getPassword());
+            psts.setInt(3, newUser.getWishlistId());
+
+            psts.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("Cannot connect to database.");
+            e.printStackTrace();
+        }
+    }
+    public void update(User user) {
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/pokedex", "root", "test");
+
+            String query = "UPDATE users " +
+                    "SET user_name=?, password=?, wishlist_id=? WHERE id=?";
+            PreparedStatement psts = conn.prepareStatement(query);
+
+            psts.setString(1, user.getUserName());
+            psts.setString(2, user.getPassword());
+            psts.setInt(3, user.getWishlistId());
+            psts.setLong(4, user.getId());
+
+            psts.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("Cannot connect to database.");
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/com/example/wishlist/ents/User.java
+++ src/main/java/com/example/wishlist/ents/User.java
@@ -4,5 +4,47 @@ public class User {
     private Long id;
     private String userName;
     private String password;
-    private Wishlist wishlist;
+    private int wishlistId;
+
+    public User() {
+    }
+
+    public User(Long id, String userName, String password, int wishlistId) {
+        this.id = id;
+        this.userName = userName;
+        this.password = password;
+        this.wishlistId = wishlistId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getWishlistId() {
+        return wishlistId;
+    }
+
+    public void setWishlistId(int wishlistId) {
+        this.wishlistId = wishlistId;
+    }
 }
